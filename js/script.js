77437217ed4024b8814af18fafbdd5e89a6dca07
Vladimir Vojnović <vladavoj@gmail.com>
2016-08-30 14:22:07 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -102,7 +102,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       
-      var chosenCategoryShortName = "'" + chooseRandomCategory(categories) + "'";
+      var chosenCategoryShortName = chooseRandomCategory();
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -135,7 +135,7 @@ function chooseRandomCategory (categories) {
   var randomArrayIndex = Math.floor(Math.random() * categories.length);
 
   // return category object with that randomArrayIndex
-  return categories[randomArrayIndex];
+  return categories[randomArrayIndex].short_name;
 }
 
 
